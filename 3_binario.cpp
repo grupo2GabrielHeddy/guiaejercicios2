3c041657574fd3ce3f912caaecf88ae5cc9af32f
Gabriel Pinto <gabriel.pinto.c@mail.pucv.cl>
2021-04-01 23:43:09 -0300

    Update 3_binario.cpp
    Corrección del código debido a que mostraba los led con orden invertidos

--- 3_binario.cpp
+++ 3_binario.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <time.h>
 #include <stdlib.h>
-
 using namespace std;
 
 int trans_binario(int decimal)
@@ -14,7 +13,7 @@ int trans_binario(int decimal)
 		binario[i] = decimal % 2;
 		decimal /= 2;
 	}
-	cout << "el numero en binario: ";
+	cout << "el numero binario es: ";
 
 	for (int i=7; i >=0; i-- )   //muestra el número binario 
 	{
@@ -25,20 +24,20 @@ int trans_binario(int decimal)
 	b4=binario[3];
 	b3=binario[2];b2=binario[1];b1=binario[0];  //Variable independiente
 	
-	printf("%d %d %d %d %d %d %d %d", b1, b2, b3, b4, b5, b6, b7, b8);   //mostralo por separado
+	printf("%d %d %d %d %d %d %d %d", b8, b7, b6, b5, b4, b3, b2, b1);   //se muestra por pantalla por separado
 
 	cout<<endl;
-	for (int i=0;i<8; i++)
+	for (int i=7;i>=0; i--)
 {
 	if (binario[i]== 0)
 		{
 		
-		printf("El LED %d esta OFF \n", i+1);
+		printf("El LED %d esta OFF \n",8-i);
 		
 		}
 	else
 		{
-		printf("El LED %d esta ON \n", i+1);
+		printf("El LED %d esta ON \n",8-i);
 		}
 	}
 	
@@ -48,16 +47,16 @@ int trans_binario(int decimal)
 }
 
 int main() {
-	int numeros[256];
-	srand(time(NULL));
+	
+    int numeros[256];
+    srand(time(NULL));
     
 	for (int i=0; i<256; i++)    
 	{
+	
 		numeros[i]= rand()% 255;
-		cout<<"El numero ";
-		cout<<i+1;
-		cout<< " es: ";
-		cout<< numeros[i];
+		cout<<"El numero es: ";
+		cout<< numeros[i];	
 		cout<<endl;
 		trans_binario(numeros[i]);
 	}
